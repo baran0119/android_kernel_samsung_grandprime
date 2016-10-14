2d39ed026cb6a3258247665efe588db57326eacb
baran0119 <baran0119@gmail.com>
2016-10-14 18:48:57 +0200

    build_kernel.sh: my own update
    Change-Id: I6fe1fd9c34c90f1743f6083b3954f3cff4a38bb7

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 export ARCH=arm
-export CROSS_COMPILE=arm-eabi-
-mkdir output
+#export CROSS_COMPILE=...
+outDir="../output"
 
-make -C $(pwd) O=output msm8916_sec_defconfig VARIANT_DEFCONFIG=msm8916_sec_fortuna_tmo_defconfig SELINUX_DEFCONFIG=selinux_defconfig
-make -C $(pwd) O=output
+# always clean build
+if [ -d ${outDir} ]; then
+	rm -fr ${outDir}
+fi
 
-cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
+mkdir ${outDir}
+
+make -C $(pwd) O=${outDir} msm8916_sec_defconfig VARIANT_DEFCONFIG=msm8916_sec_fortuna_eur_defconfig SELINUX_DEFCONFIG=selinux_defconfig
+time make -j4 -C $(pwd) O=${outDir}
